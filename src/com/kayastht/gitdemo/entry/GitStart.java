09716e1f951c4cbc3e20fa83ad79606ed10b5e29
kaytan <kaytan2394@gmail.com>
2016-09-13 18:25:04 +0530

    GitDemo: modification to GitStart.java

--- src/com/kayastht/gitdemo/entry/GitStart.java
+++ src/com/kayastht/gitdemo/entry/GitStart.java
@@ -4,6 +4,7 @@ public class GitStart {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, this is to test git repository...!");
+		System.out.println("Another modification");
 	}
 
 }
