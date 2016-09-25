00afa39a566df7e5943d84aac6b7f870c3537170
kaytan <kaytan2394@gmail.com>
2016-09-25 22:50:51 +0530

    Modification to GitStart.java
    Added print statement to indicate a modification

--- src/com/kayastht/gitdemo/entry/GitStart.java
+++ src/com/kayastht/gitdemo/entry/GitStart.java
@@ -5,6 +5,7 @@ public class GitStart {
 	public static void main(String[] args) {
 		System.out.println("Hi, this is to test git repository...!");
 		System.out.println("Another modification");
+		System.out.println("Yet another modification!");
 	}
 
 }
